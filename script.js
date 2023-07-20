8a8e536a5eb04a2364a72614a4ca1d388dbf3963
Daria Nepochatova <darena2506@mail.ru>
2023-07-20 11:18:27 +0600

    версия 1

---
+++ script.js
@@ -0,0 +1,32 @@
+let popup = document.querySelector('.popup');
+let editButton = document.querySelector('.profile__edit-button');
+let closeButton = document.querySelector('.popup__close-button');
+
+function editClick() {
+    popup.classList.add('popup_opened');
+}
+
+function closeClick() {
+    popup.classList.remove('popup_opened');
+    }
+
+editButton.addEventListener('click', editClick); 
+closeButton.addEventListener('click', closeClick); 
+
+let formElement = document.querySelector('.popup__form');
+let nameInput = document.querySelector('.popup__form-name');
+let jobInput = document.querySelector('.popup__form-job');
+let addButton = document.querySelector('.popup__form-button');
+
+function handleFormSubmit (evt) {
+    evt.preventDefault();
+    let name = document.querySelector('.profile__user-name');
+    let job = document.querySelector('.profile__about-me');
+    name.textContent = nameInput.value;
+    job.textContent = jobInput.value;
+
+}
+
+formElement.addEventListener('submit', handleFormSubmit); 
+addButton.addEventListener('click', closeClick); 
+
