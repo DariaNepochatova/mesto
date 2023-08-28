4f296d09d6f7eb6e980a955134adefe857182997
Daria Nepochatova <darena2506@mail.ru>
2023-08-28 11:33:24 +0300

    исправлены замечания пр6

--- scripts/index.js
+++ scripts/index.js
@@ -5,23 +5,6 @@ const profilePopupCloseButton = popup.querySelector(".popup__close-button");
 
 editButton.addEventListener("click", function() {
 openPopup(popup)});
-profilePopupCloseButton.addEventListener("click", function() {
-closePopup(popup)});
-
-//закрытие с помощью клика вне окна
-function popupCloseOverlay(evt) {
-  if (evt.target === evt.currentTarget) {
-    closePopup(evt.target)
-  }
-}
-
-//зактытие с помощью Esc
-function keyHandlerPopup(evt) {
-  if (evt.key === 'Escape') {
-  const currentOpenPopup = document.querySelector('.popup_opened');
-  closePopup(currentOpenPopup)
-  }
-}
 
 //попап с добавлением картинок
 const popupPicture = document.querySelector("#picture");
@@ -30,21 +13,37 @@ const addPicturePopupCloseButton = popupPicture.querySelector(".popup__close-but
 
 addPictureButton.addEventListener("click", function() {
   openPopup(popupPicture)});
-addPicturePopupCloseButton.addEventListener("click", function() { 
-  closePopup(popupPicture)});
 
 //функции для открытия и закрытия попапа
 function openPopup(popupElement) {
   popupElement.classList.add("popup_opened");
   document.addEventListener('keydown', keyHandlerPopup);
-  //для закрытия через клик вне окна
-  popupElement.addEventListener('mousedown', popupCloseOverlay)
 }
 
 function closePopup(popupElement) {
   popupElement.classList.remove("popup_opened");
   //через Esc
   document.removeEventListener('keydown', keyHandlerPopup);
+}
+  
+//для закрытия через клик вне окна
+ const popupList = Array.from(document.querySelectorAll('.popup')); // найдем все попапы на странице
+
+ popupList.forEach((popup) => { // итерируем массив. объявляя каждый попап в переменную popup
+  popup.addEventListener('mouseup', (event) => { // на каждый попап устанавливаем слушателя события
+    const targetClassList = event.target.classList; // запишем в переменную класс элемента, на котором произошло событие
+    if (targetClassList.contains('popup') || targetClassList.contains('popup__close-button')) { // проверяем наличие класса попапа ИЛИ кнопки закрытия
+      closePopup(popup); // если один из классов присутствует, то закрываем попап
+    }
+  })
+})
+
+//зактытие с помощью Esc
+function keyHandlerPopup(evt) {
+  if (evt.key === 'Escape') {
+  const currentOpenPopup = document.querySelector('.popup_opened');
+  closePopup(currentOpenPopup)
+  }
 }
 
 //находим форму для имени и работы
@@ -59,104 +58,14 @@ const placeName = popupPicture.querySelector("#place-name");
 const placeLink = popupPicture.querySelector("#place-link");
 const placeAddButton = popupPicture.querySelector(".popup__form-button");
 
-//валидация формы
+//валидация формы (validation.js)
 
-//показывает ошибку и выделяет область красным
-const showImputError = (formElement, inputElement, errorMessage) => {
-
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
-  inputElement.classList.add('popup__form-item_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__input-error');
-};
-
-//убирает показ ошибки
-const hideImputError = (formElement, inputElement) => {
-
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-
-  inputElement.classList.remove('popup__form-item_error');
-  errorElement.classList.remove('popup__input-error');
-  errorElement.textContent = '';
-};
-
-//проверка валидности формы
-const isValid = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showImputError(formElement, inputElement, inputElement.validationMessage);
-  }
-  else {
-    hideImputError(formElement, inputElement);
-  }
-};
-
-//ищет все поля в форме
-const setEventListeners = (formElement) => {
-  // Находим все поля внутри формы, сделаем из них массив методом Array.from
-  const inputList = Array.from(formElement.querySelectorAll('.popup__form-item'));
-
-  const buttonElement = formElement.querySelector(".popup__form-button");
-
-  toggleButtonState(inputList, buttonElement);
-
-  // Обойдём все элементы полученной коллекции
-  inputList.forEach((inputElement) => {
-    // каждому полю добавим обработчик события input
-    inputElement.addEventListener('input', () => {
-      // Внутри колбэка вызовем isValid, передав ей форму и проверяемый элемент
-      isValid(formElement, inputElement)
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-}; 
-
-//проверяем, есть ли невалидные инпуты в формах
-const hasInvalidInput = (inputList) => {
-  // проходим по этому массиву методом some
-  return inputList.some((inputElement) => {
-    // Если поле не валидно, колбэк вернёт true
-    // Обход массива прекратится и вся функция
-    // hasInvalidInput вернёт true
-
-    return !inputElement.validity.valid;
-  })
-}; 
-
-// Функция принимает массив полей ввода и элемент кнопки, состояние которой нужно менять
-const toggleButtonState = (inputList, buttonElement) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
-    buttonElement.classList.add('popup__form-button_inactive');
-    buttonElement.setAttribute("disabled", "disabled");
-  } else {
-    // иначе сделай кнопку активной
-    buttonElement.classList.remove('popup__form-button_inactive');
-    buttonElement.removeAttribute("disabled", "disabled");
-  }
-};
-
-//ищет все формы на странице
-const enableValidation = () => {
-  // Найдём все формы с указанным классом в DOM, сделаем из них массив методом Array.from
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-
-  // Переберём полученную коллекцию
-  formList.forEach((formElement) => {
-    // Для каждой формы вызовем функцию setEventListeners, передав ей элемент формы
-    setEventListeners(formElement);
-  });
-};
-
-// Вызовем функцию
-enableValidation(); 
+const name = document.querySelector(".profile__user-name");
+const job = document.querySelector(".profile__about-me");
 
 //функция редактирования имени и работы
 function handleProfileFormSubmit(evt) {
   evt.preventDefault();
-  const name = document.querySelector(".profile__user-name");
-  const job = document.querySelector(".profile__about-me");
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
 }
@@ -172,8 +81,7 @@ function addPicForm(evt) {
   const cardData = { name: placeName.value, link: placeLink.value };
   evt.target.reset();
   prependCardToGallery(cardData);
-  placeAddButton.setAttribute("disabled", "disabled");
-  placeAddButton.classList.add('popup__form-button_inactive');
+  inactiveButton(placeAddButton, configForm);
 }
 
 //вызов функции по добавлени карточки и закрытие формы
@@ -214,6 +122,10 @@ function prependCardToGallery(cardData) {
   gallery.prepend(cardElement);
 }
 
+const picture = document.querySelector(".popup_picture");
+const pictureImg = picture.querySelector(".popup__image");
+const pictureTitle = picture.querySelector(".popup__image-title");
+
 // функциональность карточек
 function addListenersToCard(card, cardData) {
   //лайки
@@ -225,21 +137,15 @@ function addListenersToCard(card, cardData) {
   card.querySelector(".gallery__button").addEventListener("click", function() {
       card.remove();
   });
-
-  const picture = document.querySelector(".popup_picture");
   
   //открытие попапа с картинкой
   card.querySelector(".gallery__image-container").addEventListener("click", function() {
-  picture.querySelector(".popup__image").src = cardData.link;
-  picture.querySelector(".popup__image").alt = cardData.name;
-  picture.querySelector(".popup__image-title").textContent = cardData.name;
+  pictureImg.src = cardData.link;
+  pictureImg.alt = cardData.name;
+  pictureTitle.textContent = cardData.name;
   
   openPopup(picture)
 });
-
-  //закрытие попапа с картинкой
-  picture.querySelector(".popup__close-button").addEventListener("click", function() {closePopup(picture)});
-  //picture.querySelector(".popup__overlay").addEventListener("click", function() {closePopup(picture)});
 }
 
 //создание первоначального списка карточек
@@ -252,7 +158,4 @@ function renderInitialCards() {
 }
 
 renderInitialCards();
-
-
-//валидация формы
 
